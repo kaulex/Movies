3c3e11c06c61fb380d851836095ac186327e1e49
Kaupp Alexis <kaupp.alexis@hotmail.fr>
2021-08-09 14:23:43 +0200

    chapitre: projet film part2

--- src/components/movie-details/MovieDetails.js
+++ src/components/movie-details/MovieDetails.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class MovieDetails extends Component {
-    render() {
-        return (
-            <div className="w-25 border p-4">
-               
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="w-25 border p-4 d-flex flex-column">
+        <h5>{this.props.movie.title}</h5>
+        <hr className="w-100" />
+        <div>
+          <img className="d-block mx-auto" src={this.props.movie.img} alt="cover"></img>
+        </div>
+        <hr className="w-100" />
+        <span className="text-secondary">{this.props.movie.details}</span>
+        <hr className="w-100" />
+        <span>{this.props.movie.description}</span>
+      </div>
+    );
+  }
 }
--- src/components/movie-list/MovieElement.js
+++ src/components/movie-list/MovieElement.js
@@ -1,27 +1,19 @@
 import React, { Component } from "react";
 
 export default class MovieElement extends Component {
+  mouseEnter = () => {
+    this.props.updateSelectedMovie(this.props.movie.title);
+  };
+
   render() {
     return (
-      <div className="w-25 p-2">
-        <div className="card">
-          <img
-            src="https://thecanniballecteur.files.wordpress.com/2018/05/49226cc2be5cb537218993fa89d075a8.jpg?w=584&h=876"
-            alt="film"
-            className="card-img-top"
-          />
-          <div className="card-body">
-            <h5 className="card-title">GLADIATOR</h5>
-            <p className="card-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-              ullamcorper, ligula vitae pharetra tincidunt, nisi diam interdum
-              tellus, at ultrices felis elit vitae ex. Quisque erat arcu,
-              laoreet eget consectetur vel, accumsan sit amet neque. Quisque nec
-              pharetra risus. Suspendisse ut posuere dui. Ut metus purus, congue
-              eget ipsum sed, pulvinar sodales mi. Nullam nec tellus ac lorem
-              ornare vulputate eu in magna. Phasellus orci nunc, hendrerit nec
-              euismod a, bibendum id metus.
-            </p>
+      <div onMouseEnter={this.mouseEnter} className="w-50 p-2">
+        <div className="border d-flex">
+          <img src={this.props.movie.img} alt="film" width="150" height="200" />
+          <div className="flex-fill d-flex flex-column p-3">
+            <h5>{this.props.movie.title}</h5>
+            <hr className="w-100" />
+            <span>{this.props.movie.details}</span>
           </div>
         </div>
       </div>
