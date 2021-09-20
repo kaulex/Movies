290fd8230a0820c406c33a9b3ed76a1aac207893
Kaupp Alexis <kaupp.alexis@hotmail.fr>
2021-09-20 11:30:15 +0200

    projet film partie 3

---
+++ src/components/utils/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loading() {
+    return (
+        <div style={{minHeight: '100vh'}} className="d-flex flex-row justify-content-center align-items-center w-100">
+            <img alt="loading gif" src="https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif"></img>
+        </div>
+    )
+}
